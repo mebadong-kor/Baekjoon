de3bb5ee9b915c760c1ce459a37a22a1ed48d39c
mebadong-kor <c.mebadong@gmail.com>
2020-06-02 10:57:12 +0900

    2020.06.02

---
+++ Baekjoon/5단계 - 실습 1/10996.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+/**
+ * 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n * 2; j++) {
+                if (j == n + (n % 2)) sb.append("\n");
+                else {
+                    if (j <= n) {
+                        if (j % 2 != 0) {
+                            sb.append("*");
+                        } else {
+                            sb.append(" ");
+                        }
+                    } else {
+                        if (j % 2 == 0) {
+                            sb.append("*");
+                        } else {
+                            sb.append(" ");
+                        }
+                    }
+                }
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+}
