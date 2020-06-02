a23bec94189e57db728ec8a357b8612bdea996a4
mebadong-kor <c.mebadong@gmail.com>
2020-06-02 10:35:11 +0900

    2020.06.02

--- Baekjoon/5단계 - 실습 1/2446.java
+++ Baekjoon/5단계 - 실습 1/2446.java
@@ -6,6 +6,25 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
 
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n * 2 - 1; i++) {
+            for (int j = 0; j < i - ((i - (n - 1)) * 2 * (i / n)); j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < (2 * n) - (1 + (2 * (i - ((i - (n - 1)) * 2 * (i / n))))); j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
     }
 }
