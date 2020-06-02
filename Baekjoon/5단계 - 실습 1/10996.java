49c16703c0466dbcd5bdb81ee0ec7105885fedbb
mebadong-kor <c.mebadong@gmail.com>
2020-06-02 13:34:13 +0900

    StringBuffer -> StringBuilder 변환

--- Baekjoon/5단계 - 실습 1/10996.java
+++ Baekjoon/5단계 - 실습 1/10996.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
 
---
+++ Baekjoon/6단계 - 1차원 배열/10818.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+/**
+ * N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
